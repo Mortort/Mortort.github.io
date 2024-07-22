9afc6afac13d5abde16a5ea844d4057c5b3379da
Mortort <notmortonnotott@gmail.com>
2024-07-21 21:21:04 -0500

    working on snake

--- p5jstest/sketch.js
+++ p5jstest/sketch.js
@@ -1,8 +1,14 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     s = new snake;
+    s.newdir();
     s.grow();
     s.update();
+    a = new apple;
+    a.eat();
+
+    // Detect if the device is mobile
+    isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 }
 
 function snake() {
@@ -21,27 +27,31 @@ function snake() {
         }
     }
 
-    this.grow = function() {
+    this.newdir = function(){
         this.x = this.xPositions[0];
         this.y = this.yPositions[0];
 
         // Calculate the direction vector from the current position to the mouse position
-        let dx = mouseX - this.x;
-        let dy = mouseY - this.y;
+        this.dx = mouseX - this.x;
+        this.dy = mouseY - this.y;
 
         // Calculate the distance to the mouse
-        let distance = sqrt(dx * dx + dy * dy);
+        let distance = sqrt(this.dx * this.dx + this.dy * this.dy);
 
         // Normalize the direction vector and scale by the desired speed
         let speed = this.speed;
         if (distance > 0) { // Prevent division by zero
-            dx = (dx / distance) * speed;
-            dy = (dy / distance) * speed;
+            this.dx = (this.dx / distance) * speed;
+            this.dy = (this.dy / distance) * speed;
         }
+    }
+    this.grow = function() {
+        this.x = this.xPositions[0];
+        this.y = this.yPositions[0];
 
         // Update positions
-        this.xPositions.unshift(this.x + dx);
-        this.yPositions.unshift(this.y + dy);
+        this.xPositions.unshift(this.x + this.dx);
+        this.yPositions.unshift(this.y + this.dy);
 }
 
     this.update = function(){
@@ -51,12 +61,37 @@ function snake() {
 
 }
 
+function apple(){
+    this.x;
+    this.y;
+
+    this.eat = function(){
+        this.x = random(10,windowWidth-10);
+        this.y = random(10,windowHeight-10)
+    }
+    this.draw = function(){
+        fill(color(255,0,0));
+        circle(this.x,this.y,25);
+    }
+}
+
 function draw() {
     background(220);
+    if(isMobile){
+        if(mouseIsPressed){
+            s.newdir();
+        }   
+    } else {
+        s.newdir();
+    }
     s.grow();
     s.update();
     s.draw();
-    if(mouseIsPressed){
-        s.grow();
+    a.draw();
+    if(dist(s.x, s.y, a.x, a.y)<25){
+        a.eat();
+        for(let i = 0; i<5; i++){
+            s.grow();
+        }
     }
 }
